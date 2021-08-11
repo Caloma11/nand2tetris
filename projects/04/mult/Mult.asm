73034822aa0b667d681d21395b70e878c825eb99
gianluca perrone <perronegian@hotmail.com>
2021-08-11 15:29:27 +0800

    Machine Language multiplier and some permissions

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,60 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+
+  @R2 // alocate a space for the sum
+  M=0// set R2 to start at 0
+  @14 // i refers to some mem. location.
+  M=1 // i=1
+
+
+  (LOOP)
+
+  @14 // Grab i
+
+  D=M // D=i
+
+  @R1 // grab R1
+
+  D=D-M // D=i-R1
+
+  @END
+
+  D;JGT // If (i-R1)>0 goto END
+
+  // ELSE
+
+  @R0 // Grab R1
+
+  D=M // D=R1
+
+  @R2
+
+  M=D+M // R2=R2+R0
+
+  @14 // Grab i
+
+  M=M+1 // i=i+1
+
+  @LOOP
+
+  0;JMP // Goto LOOP
+
+  (END)
+
+  @END
+
+  0;JMP // Infinite loop
+
+
+
+
+
+
+
+
+
+
+
